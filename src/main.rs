eed91420d7ee5e00a374ee4d5dc05c29ecdced19
Sergey Djumageldiev <lutlut999@outlook.com>
2020-03-07 22:03:32 +0300

    Fix cfg syntax and add test

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
-//use relative_path::RelativePath;
+use relative_path::RelativePath;
 use std::path::Path;
-//use std::io;
+use std::env;
 use std::fs;
-//use reqwest; //TODO: Simplify it
+//use std::io;
+//use reqwest; 
+
+fn main() {
+
+}
 
 #[derive(Debug)]
+
 struct Program {
     name: String,
     url: String,
     path: String, //Path to exe
     silent_key: String
 }
+
 impl Program {
     fn change_path(&mut self, path: &String) {
         self.path = path.to_owned();
-    }
-}
-
-fn main() {
-    let path: String = String::from("C:/Users/lutlu/Documents/Projects/auto_install/src/list.cfg");
-    let path = Path::new(&path);
-    let mut progs: Vec<Program> = file_to_vector(&path); 
-    for prog in progs {
-        println!("{:?}", prog);
     }
 }
 
@@ -37,23 +35,28 @@ fn file_to_vector(path: &Path) -> Vec<Program> {
         let rows = string_to_pure_rows(&file_content);
         for row in rows {
             let tokens: Vec<&str> = row.split(' ').collect(); 
-            let name = tokens[0];
-            let url = tokens[1];
-            let mut key: String = String::new();
-
-            for i in 2..tokens.len() { //For multi-args silent keys
-                key += " ";
-                key += tokens[i];
-            }
-
-            programs_list.push(
-                Program {
-                    name: name.to_owned(), 
-                    url: url.to_owned(),
-                    silent_key: key,
-                    path: String::new()
+            if tokens.len() > 1 {
+                let name = tokens[0];
+                let url = tokens[1];
+                let mut key: String = String::new();
+                if tokens.len() > 2 {
+                    key += tokens[2];
+                    for i in 3..tokens.len() { //For multi-args silent keys
+                        key += " ";
+                        key += tokens[i];
+                    }
                 }
-            )
+
+                programs_list.push(
+                    Program {
+                        name: name.to_owned(), 
+                        url: url.to_owned(),
+                        silent_key: key,
+                        path: String::new()
+                    }
+                )
+            }
+            //TODO: Installation from local
         }
     }
 
@@ -72,13 +75,23 @@ fn string_to_pure_rows(text: &String) -> Vec<String> {
     for i in 0..rows.len() { //Deletes syntax symbols
         rows[i] = rows[i]
         .trim()
-        .replace('[', "")
-        .replace(']', "")
-        .replace(',', "")
-        .replace('\"', "")
-        .replace('=', "")
+        .replace("->", "")
         .replace("  ", " ");
     }
 
     rows
+}
+
+#[cfg(test)]
+#[test]
+fn read_test() {
+    let dir = env::current_dir().unwrap();
+    let dir = dir.to_str().unwrap();
+    let path = RelativePath::new("/src/list.cfg").to_path(Path::new(&dir)); 
+    let mut progs: Vec<Program> = file_to_vector(&path);
+    let path: String = String::from(path.to_str().unwrap());
+    progs[0].change_path(&path);
+    for prog in progs {
+        println!("{:?}", prog);
+    }
 }
